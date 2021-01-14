f697000ea22ff3a80f87c8bad26e0ba2fa21d29a
c4az6 <1026037632@qq.com>
2021-01-14 08:15:19 +0800

    完成用户登录部分开发

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+// 创建一个名字叫request的axios实例，和axios本身的功能一样
+// 推荐使用axios.create方式创建实例，因为如果一个系统里面有多个api地址的时候可以给不同的实例设置不同地址
+// 如果直接使用axios.default.baseurl的方式就只能设置一个api地址，很局限
 const request = axios.create({
   baseURL: 'http://api-toutiao-web.itheima.net'
 })
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="login-container">
-    <el-form ref="form" class="login-form" :model="user">
-      <el-form-item>
-        <div class="login-form-logo"></div>
-      </el-form-item>
-      <el-form-item>
-        <el-input v-model="user.mobile" prefix-icon="el-icon-search" placeholder="请输入手机号"></el-input>
+    <el-form ref="loginRules" :rules="loginRules" class="login-form" :model="user">
+      <div class="login-form-logo"></div>
+      <el-form-item prop="mobile">
+        <el-input
+          v-model="user.mobile"
+          placeholder="请输入手机号"
+        ></el-input>
       </el-form-item>
 
-      <el-form-item>
+      <el-form-item  prop="code">
         <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
       </el-form-item>
 
-      <el-form-item>
-        <el-checkbox v-model="agree"
+      <el-form-item prop="agree">
+        <el-checkbox v-model="user.agree"
           >我已阅读并同意用户协议和隐私条款</el-checkbox
         >
       </el-form-item>
 
       <el-form-item>
-        <el-button class="login-btn" :loading=isLoading type="primary" @click="onLogin"
+        <el-button
+          class="login-btn"
+          :loading="isLoading"
+          type="primary"
+          @click="onLogin"
           >登录</el-button
         >
       </el-form-item>
@@ -28,34 +33,69 @@
 </template>
 
 <script>
-import request from '@/utils/request'
+import { userLogin } from '@/api/user'
 export default {
   name: 'LoginIndex',
   data () {
     return {
       user: {
         mobile: '13911111111', // 用户手机号
-        code: '246810' // 用户手机号验证码
+        code: '246810', // 用户手机号验证码
+        agree: false // 用户是否勾选同意协议
       },
-      agree: false, // 用户是否勾选同意协议
-      isLoading: false // 是否显示加载状态
+      isLoading: false, // 是否显示加载状态
+      // 登录表单验证规则
+      loginRules: {
+        mobile: [
+          { required: true, message: '手机号必填', trigger: 'change' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '请输入合法的手机号', trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '验证码必填', trigger: 'change' },
+          { pattern: /^\d{6}$/, message: '长度在6个数字', trigger: 'change' }
+        ],
+        agree: [
+          // 自定义表单验证
+          {
+            validator: (rule, value, callback) => {
+              if (!value) {
+                callback(new Error('请勾选用户协议'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ]
+      }
     }
   },
   methods: {
-    onLogin () {
+    // 登录请求函数
+    login () {
+      // 开启等待状态
       this.isLoading = true
-      request({
-        method: 'POST',
-        url: '/mp/v1_0/authorizations',
-        data: this.user
-      }).then(({ data }) => {
-        this.isLoading = false
-        console.log(data)
-        this.$message.success('登录成功!')
-      }).catch(error => {
-        this.isLoading = false
-        console.log(error)
-        this.$message.error('用户名或密码错误!')
+      userLogin(this.user)
+        .then(({ data }) => {
+          this.isLoading = false
+          console.log(data)
+          this.$message.success('登录成功!')
+        })
+        .catch((error) => {
+          this.isLoading = false
+          console.log(error)
+          this.$message.error('用户名或密码错误!')
+        })
+    },
+    // 用户登录事件处理函数
+    onLogin () {
+      // 表单验证
+      this.$refs.loginRules.validate((valid) => {
+        if (valid) {
+          // 验证成功
+          this.login()
+        } else {
+          return false
+        }
       })
     }
   }
@@ -79,10 +119,11 @@ export default {
     width: 259px;
     height: 57px;
     background: url("./logo_index.png") no-repeat center;
-    background-size: 70%;
+    background-size: 60%;
+    margin-bottom: 10px;
   }
   .login-form {
-    min-width: 400px;
+    min-width: 300px;
     background-color: #ffffff;
     padding: 20px 60px;
     .login-btn {
