095c72a7a6d2f2bccbc7ae005d00bfacb7ed3717
c4az6 <1026037632@qq.com>
2021-01-26 18:33:08 +0800

    完成发布文章静态页面布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,22 +1,89 @@
 <template>
-  <div class="publish-container">文章发布组件</div>
+  <div class="publish-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>发布文章</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="form.desc"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="单图"></el-radio>
+            <el-radio label="三图"></el-radio>
+            <el-radio label="无图"></el-radio>
+            <el-radio label="自动"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择频道">
+            <el-option
+             :label="item.name"
+             :value="item.id"
+             v-for="(item, index) of channels"
+             :key="index"
+             ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">发布文章</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import { articleChannel } from '@/api/article'
 export default {
   name: 'PublishIndex',
   components: {},
   props: {},
   data () {
     return {
-      artileParams: {} // 文章参数
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      channels: [] // 频道列表
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.getArticleChannel()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    // 获取频道数据函数
+    getArticleChannel () {
+      articleChannel().then(({ data: { data: { channels: res } } }) => {
+        console.log('频道数据', res)
+        this.channels = res
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    // 表单数据提交
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
