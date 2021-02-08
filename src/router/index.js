baac7230f00744354a44a44f8578af7f2506f049
c4az6 <1026037632@qq.com>
2021-02-09 00:13:19 +0800

    添加粉丝管理组件和路由

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Publish from '@/views/publish'
 import Material from '@/views/material'
 import Comment from '@/views/comment'
 import Setting from '@/views/setting'
+import Fans from '@/views/fans'
 Vue.use(VueRouter)
 
 const routes = [{
@@ -43,6 +44,10 @@ const routes = [{
     path: '/setting',
     name: 'setting',
     component: Setting
+  }, {
+    path: '/fans',
+    name: 'fans',
+    component: Fans
   }
   ]
 }
